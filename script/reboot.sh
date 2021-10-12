09be7e29748452240df17df08819a3b74e931f65
Michael Friderich <michael@frideri.ch>
2021-10-12 08:42:22 +0200

    add reboot.sh script

---
+++ script/reboot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "=========================================="
+echo ""
+echo " mhfmhf   mhfmhf  mhf      mhf  mhfmhfmhf "
+echo " mhf mhf mhf mhf  mhf      mhf  mhf       "
+echo " mhf   mhf   mhf  mhfmhfmhfmhf  mhfmhf    "
+echo " mhf         mhf  mhf      mhf  mhf       "
+echo " mhf         mhf  mhf      mhf  mhf       "
+echo ""
+echo "Good, let's make a reboot"
+echo ""
+
+sudo pacman -Syu
+sudo reboot
+
+echo ""
+echo "Good, we're done"
+echo ""
+echo "=========================================="
