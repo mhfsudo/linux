b1ddc95aad96c011e79c932d45ca603ee5d35ab5
Michael Friderich <michael@frideri.ch>
2021-10-16 08:45:59 +0200

    update install.sh

--- script/install.sh
+++ script/install.sh
@@ -11,355 +11,398 @@ echo ""
 echo "Good, let us install some programs"
 echo "first we update the system"
 echo ""
-echo "=========="
+echo "=========================================="
+echo ""
 
-echo "=========="
 cd Downloads
-ln -s /var/lib/snapd/snap /snap 
+ln -s /var/lib/snapd/snap /snap
 sudo pacman -Syu
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Anaconda for Python(y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		pacman -Sy libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl  alsa-lib libglvnd
+			pacman -Sy libxau libxi libxss libxtst libxcursor libxcomposite libxdamage libxfixes libxrandr libxrender mesa-libgl  alsa-lib libglvnd
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Atom Text Editor (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S atom
+			sudo pacman -S atom
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Authy (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install authy
+			sudo snap install authy
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Bitwarden Password Manager (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install bitwarden
+			sudo snap install bitwarden
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Brave (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S brave-browser
+			sudo pacman -S brave-browser
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Cheese Webcam (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S cheese
+			sudo pacman -S cheese
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Chrome (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		firefox https://www.google.com
+			firefox https://www.google.com
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Chromium (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S chromium
+			sudo pacman -S chromium
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Dconf Editor (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S dconf-editor
+			sudo pacman -S dconf-editor
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Deja Dup Backup Tool (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S deja-dup
+			sudo pacman -S deja-dup
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Docker (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S dockery
-		docker version
+			sudo pacman -S dockery
+			docker version
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Draw.io (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install drawio
+			sudo snap install drawio
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Java OpenJDK 14 (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
-		java -version
+			sudo pacman -S jre-openjdk-headless jre-openjdk jdk-openjdk openjdk-doc openjdk-src
+			java -version
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Eclipse IDE (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install eclipse --classic
+			sudo snap install eclipse --classic
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Fish Shell (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S fish
+			sudo pacman -S fish
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Flameshot Screenshot Tool (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S flameshot
+			sudo pacman -S flameshot
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Gimp (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S gimp
+			sudo pacman -S gimp
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Gradle (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S gradle
-		gradle -v
+			sudo pacman -S gradle
+			gradle -v
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Groovy (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S groovy
-		groovy -version
+			sudo pacman -S groovy
+			groovy -version
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "GitKraken (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install gitkraken --classic
+			sudo snap install gitkraken --classic
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Hyper Terminal (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		firefox https://releases.hyper.is/download/AppImage
-		sudo pacman -S appimagelauncher
+			firefox https://releases.hyper.is/download/AppImage
+			sudo pacman -S appimagelauncher
 		;;
 	n) echo "wird nicht installiert"
 		;;
-esa
+esac
 
-echo "=========="
+clear
+echo "=========================================="
+echo "Helm (y/n)"
+read answer
+case "$answer" in
+	y) echo "wird installiert"
+			sudo snap install helm --classic
+		;;
+	n) echo "wird nicht installiert"
+		;;
+esac
+
+clear
+echo "=========================================="
 echo "JetBrains Tools (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		firefox https://www.jetbrains.com/de-de/toolbox-app/download/download-thanks.html?platform=linux
+			firefox https://www.jetbrains.com/de-de/toolbox-app/download/download-thanks.html?platform=linux
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Jupyter Notebook for Python (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S python-pip
-		sudo pip install pip --upgrade
-		sudo pip install jupyter
-		sudo pip install jupyterlab
-		sudo pip install notebook
-		sudo pacman -S pandoc
+			sudo pacman -S python-pip
+			sudo pip install pip --upgrade
+			sudo pip install jupyter
+			sudo pip install jupyterlab
+			sudo pip install notebook
+			sudo pacman -S pandoc
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "K8s Kubectl (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install kubectl --classic
+			sudo snap install kubectl --classic
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "K8s Minikube (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S minikube
+			sudo pacman -S minikube
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "KeePass XC (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S keepassxc
+			sudo pacman -S keepassxc
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Libre Office (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S libreoffice-fresh
+			sudo pacman -S libreoffice-fresh
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Maven (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S maven
-		mvn -version
+			sudo pacman -S maven
+			mvn -version
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Microsoft Teams (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo snap install teams-insiders
+			sudo snap install teams-insiders
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "MySQL Workbench (y/n)"
 read answer
 case "$answer" in
 	y) echo "wird installiert"
-		sudo pacman -S mysql-workbench
+			sudo pacman -S mysql-workbench
 		;;
 	n) echo "wird nicht installiert"
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Noson Sonos Controller (y/n)"
 read answer
 case "$answer" in
@@ -370,7 +413,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Notepad++ Text Editor(y/n)"
 read answer
 case "$answer" in
@@ -381,7 +425,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Notion(y/n)"
 read answer
 case "$answer" in
@@ -392,7 +437,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "OneNote (y/n)"
 read answer
 case "$answer" in
@@ -403,7 +449,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Opera (y/n)"
 read answer
 case "$answer" in
@@ -414,7 +461,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Postman (y/n)"
 read answer
 case "$answer" in
@@ -425,7 +473,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "ProtonMail Bridge (y/n)"
 read answer
 case "$answer" in
@@ -436,7 +485,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "ProtonMail VPN (y/n)"
 read answer
 case "$answer" in
@@ -447,7 +497,21 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
+echo "Pyhon (y/n)"
+read answer
+case "$answer" in
+	y) echo "wird installiert"
+			sudo pacman -S python3 python3-venv python3-pip
+			python3 --version
+		;;
+	n) echo "wird nicht installiert"
+		;;
+esac
+
+clear
+echo "=========================================="
 echo "Signal (y/n)"
 read answer
 case "$answer" in
@@ -458,7 +522,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Spaceship Shell Erweiterung (y/n)"
 read answer
 case "$answer" in
@@ -469,7 +534,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Spotify (y/n)"
 read answer
 case "$answer" in
@@ -480,7 +546,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Starship Shell Erweiterung (y/n)"
 read answer
 case "$answer" in
@@ -491,7 +558,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Sublime Text (y/n)"
 read answer
 case "$answer" in
@@ -502,7 +570,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Synology Drive (y/n)"
 read answer
 case "$answer" in
@@ -513,7 +582,8 @@ case "$answer" in
     	;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Telegram (y/n)"
 read answer
 case "$answer" in
@@ -524,7 +594,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "TexMaker (y/n)"
 read answer
 case "$answer" in
@@ -535,7 +606,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Threema (y/n)"
 read answer
 case "$answer" in
@@ -546,7 +618,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Thunderbird Mail (y/n)"
 read answer
 case "$answer" in
@@ -557,7 +630,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Timeshift (y/n)"
 read answer
 case "$answer" in
@@ -568,7 +642,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Todoist (y/n)"
 read answer
 case "$answer" in
@@ -579,7 +654,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Tweaks (y/n)"
 read answer
 case "$answer" in
@@ -591,7 +667,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Vim (y/n)"
 read answer
 case "$answer" in
@@ -602,7 +679,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Virtual Box (y/n)"
 read answer
 case "$answer" in
@@ -613,7 +691,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Visual Studio Code IDE (y/n)"
 read answer
 case "$answer" in
@@ -625,7 +704,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Vivaldi Browser (y/n)"
 read answer
 case "$answer" in
@@ -636,7 +716,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "VLC Player (y/n)"
 read answer
 case "$answer" in
@@ -647,7 +728,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Whatsapp (y/n)"
 read answer
 case "$answer" in
@@ -658,7 +740,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "Wireshark (y/n)"
 read answer
 case "$answer" in
@@ -669,7 +752,8 @@ case "$answer" in
 		;;
 esac
 
-echo "=========="
+clear
+echo "=========================================="
 echo "ZSH Shell (y/n)"
 read answer
 case "$answer" in
@@ -685,23 +769,28 @@ esac
 
 sudo  pacman -Syu
 
-echo "=========="
+echo "=========================================="
 echo "Alias recommendation:"
+echo "vim ~/.bashrc"
+echo "vim ~/.config/fish/config.fish"
 echo "vim ~/.zshrc"
 echo "alias po='bash .poweroff.sh'"
 echo "alias re='bash .reboot.sh'"
 echo "alias up='bash .update.sh'"
 echo ""
-echo "=========="
+echo "=========================================="
 echo "Dconf Editor recommendation:"
 echo ""
-echo "Center Ubuntu Dock 		- org/gnome/shell/extensions/dash-to-dock (extend-height)"
-echo "Don't show mounted drives - org/gnome/shell/extensions/dash-to-dock (show-mounts)"
-echo "Dock background			- org/gnome/shell/extensions/dash-to-dock (backround-opacity)"
-echo "Icon dot color			- org/gnome/shell/extensions/dash-to-dock (custom-theme-running-dots-color (#00ff00))"
-echo "Show icon backlit 		- org/gnome/shell/extensions/dash-to-dock (unity-backlit-items)"
+echo "Center Ubuntu Dock"
+echo "org/gnome/shell/extensions/dash-to-dock/extend-height"
+echo "Don't show mounted drives"
+echo "org/gnome/shell/extensions/dash-to-dock/show-mounts"
+echo "Dock background"
+echo "org/gnome/shell/extensions/dash-to-dock/custom-theme-running-dots-color (#00ff00))"
+echo "Show icon backlit"
+echo "org/gnome/shell/extensions/dash-to-dock/unity-backlit-items"
 echo ""
-echo "=========="
+echo "=========================================="
 echo "Shell extensions recommendation"
 echo ""
 echo "https://extensions.gnome.org/extension/16/auto-move-windows/"
@@ -725,25 +814,25 @@ echo "https://extensions.gnome.org/extension/1266/transparent-top-bar/"
 echo "https://extensions.gnome.org/extension/19/user-themes/"
 echo "https://extensions.gnome.org/extension/21/workspace-indicator/"
 echo ""
-echo "=========="
+echo "=========================================="
 echo "Shortcut recommendation:"
 echo ""
-echo "Atom					Ctrl+Alt+E"
-echo "Authy					Ctrl+Alt+A"
-echo "Calculator			Ctrl+Alt+C"
-echo "Flameshot	gui			Print"
-echo "Gitkraken				Ctrl+Alt+G"
-echo "Home Folder			Ctrl+Alt+F"
+echo "Atom								Ctrl+Alt+E"
+echo "Authy								Ctrl+Alt+A"
+echo "Calculator					Ctrl+Alt+C"
+echo "Flameshot	gui				Print"
+echo "Gitkraken						Ctrl+Alt+G"
+echo "Home Folder					Ctrl+Alt+F"
 echo "Launch calculator 	Ctral+Alt+C"
 echo "Launch web browser 	Ctrl+Alt+W"
-echo "Notion				Ctrl+Alt+N"
-echo "Settings				Ctrl+Alt+S"
-echo "Spotify				Ctrl+Alt+X"
-echo "Web Browser			Ctrl+Alt+W"
-echo "VS Code				Ctrl+Alt+E"
+echo "Notion							Ctrl+Alt+N"
+echo "Settings						Ctrl+Alt+S"
+echo "Spotify							Ctrl+Alt+X"
+echo "Web Browser					Ctrl+Alt+W"
+echo "VS Code							Ctrl+Alt+E"
 echo ""
 
-echo "=========="
+echo "=========================================="
 echo "Puuhh, that was a lot of work"
 echo "Don't forget the recommendations above!"
 echo "Should we reboot the System?"
@@ -751,7 +840,7 @@ echo ""
 read answer
 case "$answer" in
   y) echo "wird neugestartet"
-  		sudo reboot
+  			sudo reboot
     	;;
   n) echo "wird nicht neugestartet"
     	;;
